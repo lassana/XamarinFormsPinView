6c9887bb2e364731fb99a94b3b0fdc2486502a98
Mikalai Daronin <lassana.nd@gmail.com>
2018-06-15 22:04:50 +0300

    Fix the project build.

--- FormsPinView/FormsPinView.Droid/Properties/AssemblyInfo.cs
+++ FormsPinView/FormsPinView.Droid/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FormsPinView.Droid")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Mikalai Daronin © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,8 +16,8 @@
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
